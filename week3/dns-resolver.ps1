b8a6d3cafdfcab81befe82b16a61e93440fc4059
Duncanqwe <70866959+Duncanqwe@users.noreply.github.com>
2023-10-16 18:53:02 -0400

    Create dns-resolver.ps1

---
+++ week3/dns-resolver.ps1
@@ -0,0 +1,21 @@
+# Takes a network prefix and your DNS server of choice as parameters
+param (
+    [string]$NetworkPrefix,
+    [string]$DnsServer
+)
+
+# Check if chosen DNS server is online
+if (-not (Test-Connection -ComputerName $DnsServer -Count 1 -Quiet)) {
+    Write-Host "($DnsServer) is not reachable"
+    Exit
+}
+
+# Make a list of IP addresses based on network prefix
+$ipRange = 0..255 | ForEach-Object {
+    "$NetworkPrefix.$_"
+}
+
+# Resolves DNS names
+$ipRange | ForEach-Object {
+    Resolve-DnsName -Name $_ -Server $DnsServer -ErrorAction Ignore
+}
